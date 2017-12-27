6e11e6e532cafd746568ad542ba6c278a4329ac7
ECXCARD\guilherme.lima <montante1!>
2017-12-27 12:19:14 -0200

    first

---
+++ src/main/java/com/guilhermefgl/spring/crudproduto/CrudProdutoImagemApplication.java
@@ -0,0 +1,12 @@
+package com.guilhermefgl.spring.crudproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProdutoImagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProdutoImagemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/guilhermefgl/spring/crudproduto/CrudProdutoImagemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guilhermefgl.spring.crudproduto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudProdutoImagemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
