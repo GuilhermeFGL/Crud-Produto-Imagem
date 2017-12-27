927688952c96a7234876fea7010923ec13c99af8
ECXCARD\guilherme.lima <montante1!>
2017-12-27 17:30:02 -0200

    db profiles

--- src/test/java/com/guilhermefgl/spring/crudproduto/models/repositories/ProdutoRepositoryTest.java
+++ src/test/java/com/guilhermefgl/spring/crudproduto/models/repositories/ProdutoRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.guilhermefgl.spring.crudproduto.models.Produto;
@@ -17,6 +18,7 @@ import com.guilhermefgl.spring.crudproduto.models.repositories.ProdutoRepository
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProdutoRepositoryTest {
 	
 	@Autowired
